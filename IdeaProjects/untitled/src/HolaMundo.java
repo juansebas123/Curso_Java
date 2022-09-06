8da1772d77679529c922a1e6f506802b8a4f74d0
Trabajo Ricaurte <trabajoricaurte@hotmail.com>
2022-09-06 17:25:57 -0500

    ejemplo trabajoricaurte push

--- IdeaProjects/untitled/src/HolaMundo.java
+++ IdeaProjects/untitled/src/HolaMundo.java
@@ -2,4 +2,5 @@ public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("hOLA PERRA");  //saut ATAJO
     }
+     static {System.out.println("hOLA trabajoricaurte");}
 }
